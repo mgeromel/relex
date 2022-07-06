7732dfe12e2ee8f9b55763887014956991db44a1
marcel <marcel.geromel@gmail.com>
2022-07-06 06:38:58 +0200

    Finalized BeamSearch

--- beam_search.py
+++ beam_search.py
@@ -1,66 +1,47 @@
-from transformers.generation_logits_process import LogitsProcessor
-from transformers.gemeration_beam_search import BeamScorer
-
 import torch.nn.functional as F
 
-def beam_search(
-	self,
-	input_ids = None,
-	num_beams = 1,
-	max_length = 64,
-	output_scores = False,
-	output_attentions = False,
-	output_hidden_states = False,
-	return_dict_in_generate = False,
-	**model_kwargs,
-):
-	##################################################
+def beam_search(self, input_ids = None, num_beams = 1, max_length = 64, **model_kwargs,):
+
+	######################################################
 
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-	##################################################
+	######################################################
 
 	# INITIALIZE VALUES
-	pad_token_id = self.config.pad_token_id # ??
-	eos_token_id = self.config.eos_token_id # ??
+	batch_size = input_ids.shape[0] # ...
+	input_size = input_ids.shape[1] # SEQUENCE LENGTH
 
-	batch_size = input_ids.shape[0]
-	input_size = input_ids.shape[1]
-
+	# "NEW" BATCH_SIZE
 	batch_beam_size = batch_size * num_beams
 	
-	##################################################
-		
+	# SYNCHRONIZED G_LOGITS --> "BATCH_SIZE" 
 	undone = torch.ones(batch_size, device=device)  # GENERATED SEQUENCES (FLAG)
 	string = [ "[BOS] #_RELS_#" ] * batch_size
 	
+	# MASK FOR G_LOGITS
 	self.mask = self.mask.to(device)
 
-	beam_input_ids = input_ids.repeat(1, 1, num_beams).view(batch_beam_size, input_size)
-
-	##################################################
-
+	# TABLE FOR BEAM-SCORES
 	beam_scores = torch.zeros((batch_size, num_beams), dtype=torch.float, device=input_ids.device)
-	beam_scores[:, 1:] = -1e9
-	beam_scores = beam_scores.view((batch_size * num_beams,))
-
-	beam_batch_size
-
-	#------------------------------------------------#
-
+	beam_scores[:, 1:] = -1e9 # MASKING, 1ST ITERATION
+	
+	#----------------------------------------------------#
 	# ENCODER: ONLY ONCE
-	attention_mask = input_ids.ne(0).long()
+
+	encoder_inputs = input_ids.repeat(1, num_beams).view(batch_size * num_beams, input_size)
+	attention_mask = encoder_input_ids.ne(0).long()
 
 	encoder_outputs = self.encoder(
-		input_ids = input_ids,
+		input_ids = encoder_inputs,
 		attention_mask = attention_mask,
 		output_hidden_states = True,
 		return_dict = True
 	)
 
-	#------------------------------------------------#
-
+	#----------------------------------------------------#
 	# DECODER: PREPARE INPUTS
+
 	decoder_input_ids = torch.zeros(
 		batch_beam_size, 1, self.vocab_size, device=device
 	).float()
@@ -68,7 +49,7 @@ def beam_search(
 	decoder_input_ids[:, :, 1] = 1
 	decoder_attention_mask = torch.ones(batch_beam_size, 1, device=device)
 
-	##################################################
+	######################################################
 
 	for length in range(max_length - 1):
 		
@@ -76,6 +57,7 @@ def beam_search(
 			break
 
 		#------------------------------------------------#
+		# DECODER FORWARD-PASS
 
 		decoder_outputs = self.forward(
 			input_ids = input_ids,
@@ -86,26 +68,26 @@ def beam_search(
 		)
 
 		#------------------------------------------------#
+		# RETRIEVE LOGITS
 
 		g_logits = decoder_outputs["logits"][:, -1,                   :   self.gramm_size].detach()
 		r_logits = decoder_outputs["logits"][:, -1,   self.gramm_size :-2*self.point_size].detach()
 		p_logits = decoder_outputs["logits"][:, -1,-2*self.point_size :                  ].detach()
 
 		#------------------------------------------------#
+		# "AVERAGE VALUES": 'G_LOGITS'
 
-		# "AVERAGE VALUES": 'g_logits'
 		g_logits = torch.sum(g_logits.view(batch_size, num_beams, self.gramm_size), dim = 1) / num_beams
 
 		#------------------------------------------------#
-
-		# GRAMMAR MASKING: 'g_logits'
+		# GRAMMAR MASKING: 'G_LOGITS'
 
 		for idx, string in enumerate(strings):
 			if undone[idx]: 
 				l_bound = string.find("#_")
 				r_bound = string.find("_#") + 2
 
-				if l_bound == -1: # DONE
+				if l_bound == -1: # DONE / ERROR
 					continue
 
 				state = string[l_bound : r_bound]
@@ -113,142 +95,100 @@ def beam_search(
 				g_logits[idx][1:] = g_logits[idx][1:] + self.mask[self.smap[state]]
 				g_logits[idx][0 ] = -float("inf")
 
-				production = self.gramm.rule(
-					torch.argmax(g_logits[idx][1:])
-				)
-
+				production = self.gramm.rule(torch.argmax(g_logits[idx][1:]))
 				strings[idx] = strings[idx].replace(state, production[1], 1)
 
 		#------------------------------------------------#
-
 		# CLEANUP / UPSCALE / CLONING:
 
 		g_logits = F.one_hot(torch.argmax(g_logits, dim = -1), self.gramm_size)
-		g_logits = g_logits.repeat(1, 1, num_beams).squeeze()
-		g_logits = g_logits.view(batch_beam_size, self.gramm_size)
+		g_logits = g_logits.repeat(1, num_beams)
 
 		#------------------------------------------------#
-
 		# BEAM-SEARCH: 'r_logits'
 
 		# shape = (batch_size, num_beams, vocab_size)
 		g_logits.view(batch_size, num_beams, self.gramm_size)
-		r_logits.view(batch_size, num_beams, self.vocab_size)
+		r_logits.view(batch_size, num_beams, self.relat_size)
 		p_logits.view(batch_size, num_beams, self.point_size * 2)
 
-		# shape = (batch_size, num_beams, ?)
-		next_scores = F.log_softmax(r_logits, dim = -1)
-		beam_scores = beam_scores.view(batch_size, num_beams)
-
-		# FOR EACH BATCH:		
+		# decoder_input_ids += (g_logits + r_logits + p_logits)
+		decoder_input_ids = decoder_input_ids.view(batch_size, num_beams, length + 1, self.vocab_size)
+		
+		# FOR EACH SAMPLE:		
 		for idx in range(batch_size):
 			if undone[idx]:
 				undone[idx] = "[EOS]" not in strings[i]
 
 				if "TOKEN" in string[idx]:
 					#------------------------------------#
-					# UPDATE BEAM_SCORES
-					next_scores[idx] = next_scores[idx] + beam_scores[idx, :, None].expand_as(next_scores)
+					# CURRENT SCORES
+					next_scores = F.log_softmax(r_logits[idx], dim = -1)
 					
-					# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-					# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-					# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-					
-					r_logits[idx] = torch.zeros(size = r_logits[idx].shape, out = r_logits[idx])
+					# TOP-K: SCORES / TOKENS
+					best_scores = next_scores + beam_scores[idx, None].expand_as(next_scores)
+					best_scores, best_tokens = torch.topk(best_scores.view(-1), num_beams, dim = 0)
+
+					# BEAM-INDEX / NEXT-TOKEN
+					beam_number = torch.div(best_tokens, self.relat_size, rounding_mode "floor")
+					best_tokens = best_tokens % self.relat_size
+
+					# RE-ORDERING OF BEAMS 
+					decoder_input_ids[idx] = decoder_input_ids[idx, beam_number]
+
+					# UPDATE BEAM SCORES
+					beam_scores[idx] = best_scores
+
+					# UPDATE DECODER_INPUT_IDS
+					r_logits[idx] = F.one_hot(best_tokens, self.relat_size)
 					p_logits[idx] = torch.zeros(size = p_logits[idx].shape, out = p_logits[idx])
 
+					# CLEANUP
 					string[idx] = string[idx].replace("TOKEN", "token")
 					#------------------------------------#
 
 				if "POINT" in string[idx]:
 					#------------------------------------#
+					# SLICE P_LOGITS IN HALF
 					l_index, r_index = p_logits[idx].split(self.point_size, dim = -1)
 					
+					# L_INDEX / R_INDEX
 					l_index = F.one_hot(torch.argmax(l_index, dim = -1), self.point_size)
 					r_index = F.one_hot(torch.argmax(r_index, dim = -1), self.point_size)
 
+					# UPDATE DECODER_INPUT_IDS
 					p_logits[idx] = torch.cat([l_index, r_index], dim = -1)
 					r_logits[idx] = torch.zeros(size=r_logits[idx].shape, out=r_logits[idx])
- 
+ 					
+ 					# CLEANUP
 					string[idx] = string[idx].replace("POINT", "point")
 					#------------------------------------#
 			else:
 				# BATCH_INPUT_IDS: PADDING
-				g_logits[idx] = F.one_hot(torch.tensor(0), self.gramm_size).repeat(num_beams, 1)
+				g_logits[idx] = F.one_hot(torch.tensor([0] * num_beams), self.gramm_size)
 				r_logits[idx] = torch.zeros(size = r_logits[idx].shape, out = r_logits[idx])
 				p_logits[idx] = torch.zeros(size = p_logits[idx].shape, out = p_logits[idx])
 
 		#------------------------------------------------#
 
-		# reshape for beam search
-		vocab_size = next_token_scores.shape[-1]
-		next_token_scores = next_token_scores.view(batch_size, num_beams * vocab_size)
+		# DECODER_INPUT_IDS
+		next_decoder_input_ids = torch.cat([g_logits, r_logits, p_logits], dim = -1)
+		decoder_input_ids = torch.cat([decoder_input_ids, next_decoder_input_ids], dim = 2)
 
-		next_token_scores, next_tokens = torch.topk(
-			next_token_scores, 2 * num_beams, dim=1, largest=True, sorted=True
-		)
+		# DECODER_ATTENTION_MASK
+		next_decoder_attention = undone.view(batch_size, 1).repeat(1, num_beams)
+		next_decoder_attention = next_decoder_attention.view(batch_size * num_beams, 1)
+		decoder_attention_mask = torch.cat([decoder_attention_mask, next_decoder_attention], dim = -1)
 
-		next_indices = torch_int_div(next_tokens, vocab_size)
-		next_tokens = next_tokens % vocab_size
+	##################################################
+	# FINALIZE OUTPUT_SEQUENCE
 
-		# stateless
-		beam_outputs = beam_scorer.process(
-			input_ids,
-			next_token_scores,
-			next_tokens,
-			next_indices,
-			pad_token_id=pad_token_id,
-			eos_token_id=eos_token_id,
-			beam_indices=beam_indices,
-		)
+	best_beams, best_index = torch.topk(beam_scores, 1, dim = -1)
 
-		beam_scores = beam_outputs["next_beam_scores"]
-		beam_next_tokens = beam_outputs["next_beam_tokens"]
-		beam_idx = beam_outputs["next_beam_indices"]
+	import IPython ; IPython.embed() ; exit(1)
 
-		decoder_input_ids = torch.cat([input_ids[beam_idx, :], beam_next_tokens.unsqueeze(-1)], dim=-1)
-		decoder_attention_mask
-
+	output = None
 
 	##################################################
 	
-	sequence_outputs = beam_scorer.finalize(
-		input_ids,
-		beam_scores,
-		next_tokens,
-		next_indices,
-		pad_token_id=pad_token_id,
-		eos_token_id=eos_token_id,
-		max_length=stopping_criteria.max_length,
-		beam_indices=beam_indices,
-	)
-
-	##################################################
-	
-	if return_dict_in_generate:
-		if not output_scores:
-			sequence_outputs["sequence_scores"] = None
-
-		if self.config.is_encoder_decoder:
-			return BeamSearchEncoderDecoderOutput(
-				sequences=sequence_outputs["sequences"],
-				sequences_scores=sequence_outputs["sequence_scores"],
-				scores=scores,
-				beam_indices=sequence_outputs["beam_indices"],
-				encoder_attentions=encoder_attentions,
-				encoder_hidden_states=encoder_hidden_states,
-				decoder_attentions=decoder_attentions,
-				cross_attentions=cross_attentions,
-				decoder_hidden_states=decoder_hidden_states,
-			)
-		else:
-			return BeamSearchDecoderOnlyOutput(
-				sequences=sequence_outputs["sequences"],
-				sequences_scores=sequence_outputs["sequence_scores"],
-				scores=scores,
-				beam_indices=sequence_outputs["beam_indices"],
-				attentions=decoder_attentions,
-				hidden_states=decoder_hidden_states,
-			)
-	else:
-		return sequence_outputs["sequences"]
+	return output
--- model.py
+++ model.py
@@ -258,3 +258,192 @@ class TestModel(torch.nn.Module):
 		return decoder_input_ids
 
 	##################################################
+
+	def beam_search(self, input_ids = None, num_beams = 1, max_length = 64, **model_kwargs,):
+
+		######################################################
+
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+		######################################################
+
+		# INITIALIZE VALUES
+		batch_size = input_ids.shape[0] # ...
+		input_size = input_ids.shape[1] # SEQUENCE LENGTH
+
+		# "NEW" BATCH_SIZE
+		batch_beam_size = batch_size * num_beams
+		
+		# SYNCHRONIZED G_LOGITS --> "BATCH_SIZE" 
+		undone = torch.ones(batch_size, device=device)  # GENERATED SEQUENCES (FLAG)
+		string = [ "[BOS] #_RELS_#" ] * batch_size
+		
+		# MASK FOR G_LOGITS
+		self.mask = self.mask.to(device)
+
+		# TABLE FOR BEAM-SCORES
+		beam_scores = torch.zeros((batch_size, num_beams), dtype=torch.float, device=input_ids.device)
+		beam_scores[:, 1:] = -1e9 # MASKING, 1ST ITERATION
+		
+		#----------------------------------------------------#
+		# ENCODER: ONLY ONCE
+
+		encoder_inputs = input_ids.repeat(1, num_beams).view(batch_size * num_beams, input_size)
+		attention_mask = encoder_input_ids.ne(0).long()
+
+		encoder_outputs = self.encoder(
+			input_ids = encoder_inputs,
+			attention_mask = attention_mask,
+			output_hidden_states = True,
+			return_dict = True
+		)
+
+		#----------------------------------------------------#
+		# DECODER: PREPARE INPUTS
+
+		decoder_input_ids = torch.zeros(
+			batch_beam_size, 1, self.vocab_size, device=device
+		).float()
+
+		decoder_input_ids[:, :, 1] = 1
+		decoder_attention_mask = torch.ones(batch_beam_size, 1, device=device)
+
+		######################################################
+
+		for length in range(max_length - 1):
+			
+			if sum(undone) == 0:
+				break
+
+			#------------------------------------------------#
+			# DECODER FORWARD-PASS
+
+			decoder_outputs = self.forward(
+				input_ids = input_ids,
+				attention_mask = attention_mask,
+				encoder_outputs = encoder_outputs,
+				decoder_input_ids = decoder_input_ids,
+				decoder_attention_mask = decoder_attention_mask
+			)
+
+			#------------------------------------------------#
+			# RETRIEVE LOGITS
+
+			g_logits = decoder_outputs["logits"][:, -1,                   :   self.gramm_size].detach()
+			r_logits = decoder_outputs["logits"][:, -1,   self.gramm_size :-2*self.point_size].detach()
+			p_logits = decoder_outputs["logits"][:, -1,-2*self.point_size :                  ].detach()
+
+			#------------------------------------------------#
+			# "AVERAGE VALUES": 'G_LOGITS'
+
+			g_logits = torch.sum(g_logits.view(batch_size, num_beams, self.gramm_size), dim = 1) / num_beams
+
+			#------------------------------------------------#
+			# GRAMMAR MASKING: 'G_LOGITS'
+
+			for idx, string in enumerate(strings):
+				if undone[idx]: 
+					l_bound = string.find("#_")
+					r_bound = string.find("_#") + 2
+
+					if l_bound == -1: # DONE / ERROR
+						continue
+
+					state = string[l_bound : r_bound]
+
+					g_logits[idx][1:] = g_logits[idx][1:] + self.mask[self.smap[state]]
+					g_logits[idx][0 ] = -float("inf")
+
+					production = self.gramm.rule(torch.argmax(g_logits[idx][1:]))
+					strings[idx] = strings[idx].replace(state, production[1], 1)
+
+			#------------------------------------------------#
+			# CLEANUP / UPSCALE / CLONING:
+
+			g_logits = F.one_hot(torch.argmax(g_logits, dim = -1), self.gramm_size)
+			g_logits = g_logits.repeat(1, num_beams)
+
+			#------------------------------------------------#
+			# BEAM-SEARCH: 'r_logits'
+
+			# shape = (batch_size, num_beams, vocab_size)
+			g_logits.view(batch_size, num_beams, self.gramm_size)
+			r_logits.view(batch_size, num_beams, self.relat_size)
+			p_logits.view(batch_size, num_beams, self.point_size * 2)
+
+			# decoder_input_ids += (g_logits + r_logits + p_logits)
+			decoder_input_ids = decoder_input_ids.view(batch_size, num_beams, length + 1, self.vocab_size)
+			
+			# FOR EACH SAMPLE:		
+			for idx in range(batch_size):
+				if undone[idx]:
+					undone[idx] = "[EOS]" not in strings[i]
+
+					if "TOKEN" in string[idx]:
+						#------------------------------------#
+						# CURRENT SCORES
+						next_scores = F.log_softmax(r_logits[idx], dim = -1)
+						
+						# TOP-K: SCORES / TOKENS
+						best_scores = next_scores + beam_scores[idx, None].expand_as(next_scores)
+						best_scores, best_tokens = torch.topk(best_scores.view(-1), num_beams, dim = 0)
+
+						# BEAM-INDEX / NEXT-TOKEN
+						beam_number = torch.div(best_tokens, self.relat_size, rounding_mode "floor")
+						best_tokens = best_tokens % self.relat_size
+
+						# RE-ORDERING OF BEAMS 
+						decoder_input_ids[idx] = decoder_input_ids[idx, beam_number]
+
+						# UPDATE BEAM SCORES
+						beam_scores[idx] = best_scores
+
+						# UPDATE DECODER_INPUT_IDS
+						r_logits[idx] = F.one_hot(best_tokens, self.relat_size)
+						p_logits[idx] = torch.zeros(size = p_logits[idx].shape, out = p_logits[idx])
+
+						# CLEANUP
+						string[idx] = string[idx].replace("TOKEN", "token")
+						#------------------------------------#
+
+					if "POINT" in string[idx]:
+						#------------------------------------#
+						# SLICE P_LOGITS IN HALF
+						l_index, r_index = p_logits[idx].split(self.point_size, dim = -1)
+						
+						# L_INDEX / R_INDEX
+						l_index = F.one_hot(torch.argmax(l_index, dim = -1), self.point_size)
+						r_index = F.one_hot(torch.argmax(r_index, dim = -1), self.point_size)
+
+						# UPDATE DECODER_INPUT_IDS
+						p_logits[idx] = torch.cat([l_index, r_index], dim = -1)
+						r_logits[idx] = torch.zeros(size=r_logits[idx].shape, out=r_logits[idx])
+	 					
+	 					# CLEANUP
+						string[idx] = string[idx].replace("POINT", "point")
+						#------------------------------------#
+				else:
+					# BATCH_INPUT_IDS: PADDING
+					g_logits[idx] = F.one_hot(torch.tensor([0] * num_beams), self.gramm_size)
+					r_logits[idx] = torch.zeros(size = r_logits[idx].shape, out = r_logits[idx])
+					p_logits[idx] = torch.zeros(size = p_logits[idx].shape, out = p_logits[idx])
+
+			#------------------------------------------------#
+
+			# DECODER_INPUT_IDS
+			next_decoder_input_ids = torch.cat([g_logits, r_logits, p_logits], dim = -1)
+			decoder_input_ids = torch.cat([decoder_input_ids, next_decoder_input_ids], dim = 2)
+
+			# DECODER_ATTENTION_MASK
+			next_decoder_attention = undone.view(batch_size, 1).repeat(1, num_beams)
+			next_decoder_attention = next_decoder_attention.view(batch_size * num_beams, 1)
+			decoder_attention_mask = torch.cat([decoder_attention_mask, next_decoder_attention], dim = -1)
+
+		##################################################
+		# FINALIZE OUTPUT_SEQUENCE
+
+		output = None
+
+		##################################################
+		
+		return output
